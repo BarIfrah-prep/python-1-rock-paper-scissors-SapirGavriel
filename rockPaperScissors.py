d6d0fb89dc4facc9e3fe210e1e8d55353344bbd1
Sapir Gavriel <sapirga@edu.hac.ac.il>
2022-06-14 21:11:00 +0300

    Hello Bar, I finish the project.

--- rockPaperScissors.py
+++ rockPaperScissors.py
@@ -44,10 +44,55 @@ print("GAME RULES: \n"
 
 # The game will run in a WHILE loop.
 # The loop is infinite, and only the user has the power to stop it (when they say they don't want to play anymore)
-while True:
-    """
-      This is the game's heart. You'll need to think and use everything we've learned so far to make this game work.
-      Remember Python's rules ( the ':' after a statement, the indentation with loops and statements..)
-      
-      """
-    pass
+
+while gameison:
+    userChoice = input("Choose your Choice rock(1),paper(2),Scissors(3)")
+
+    while userChoice != "1" and userChoice != "2" and userChoice != "3":
+        print("invalid choice")
+        userChoice = input("Choose your Choice rock(1),paper(2),Scissors(3)")
+    else:
+        computerChoice = random.randint(1,3)
+        userChoice = int(userChoice)
+
+        # case user choose rock
+        if userChoice == 1 and computerChoice == 3:
+            print("Hi, you won")
+        elif userChoice == 1 and computerChoice == 2:
+            print("Ho, you lose")
+        elif userChoice == 1 and copmuterChoice == 1:
+            print("its a draw")
+
+        # case user choose paper
+        if userChoice == 2 and computerChoice == 1:
+            print("Hi, you won")
+        elif userChoice == 2 and computerChoice == 3:
+            print("Ho, you lose")
+        elif userChoice == 2 and copmuterChoice == 2:
+            print("its a draw")
+
+        # case user choose scissors
+        if userChoice == 3 and computerChoice == 2:
+            print("Hi, you won")
+        elif userChoice == 3 and computerChoice == 1:
+            print("Ho, you lose")
+        elif userChoice == 3 and copmuterChoice == 3:
+            print("its a draw")
+
+
+        # Asking the user if he wants to continue
+
+        answerRequest = input("Do you want continue? y/n").lower()
+        continueRequest = True
+
+        while answerRequest != "Y" and answerRequest != "n":
+            print("invalid answer")
+            answerRequest = input("Do you want continue? y/n").lower()
+
+        if answerRequest == "y":
+            continue
+        elif answerRequest == "n":
+            print("thank you for playing the game!")
+            gameison = False
+
+
